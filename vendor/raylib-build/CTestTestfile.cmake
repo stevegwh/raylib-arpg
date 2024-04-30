bfcd2d916170edc1f6fb7e12cc33fab8ad8f432b
stevegwh <sgwheeler@posteo.net>
2024-04-30 20:16:23 +0100

    removed vendor folder

--- vendor/raylib-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/steve/CLionProjects/BG_Raylib/vendor/raylib-src
-# Build directory: /Users/steve/CLionProjects/BG_Raylib/vendor/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
-subdirs("examples")
