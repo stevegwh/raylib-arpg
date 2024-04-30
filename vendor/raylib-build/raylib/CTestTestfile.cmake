edbf222455b761bcbd1924ff5ce63ca0d830f928
stevegwh <sgwheeler@posteo.net>
2024-04-30 20:14:52 +0100

    Added entt as dependency

---
+++ vendor/raylib-build/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/steve/CLionProjects/BG_Raylib/vendor/raylib-src/src
+# Build directory: /Users/steve/CLionProjects/BG_Raylib/vendor/raylib-build/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/glfw")
