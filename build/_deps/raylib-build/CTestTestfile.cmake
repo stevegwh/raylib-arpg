df5c37a31201d9867c272440bfe53135e26a5691
stevegwh <sgwheeler@posteo.net>
2024-06-04 15:26:09 +0100

    Removed dirs

--- build/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/steve/CLionProjects/BG_Raylib/build/_deps/raylib-src
-# Build directory: /Users/steve/CLionProjects/BG_Raylib/build/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
-subdirs("examples")
