a21d89838e42ddd8ea7c54e6394ab1a31a082810
Steve <sgwheeler@posteo.net>
2024-07-13 20:56:49 +0100

    workiing for lights

---
+++ core/src/implementations.cpp
@@ -0,0 +1,9 @@
+// Include all header-only libraries here and define their implementations
+
+#include <raylib.h>
+//#define RAYGUI_IMPLEMENTATION
+//#include "raygui.h"
+//#define GUI_WINDOW_FILE_DIALOG_IMPLEMENTATION
+//#include "../editor/windows/gui_window_file_dialog.h"
+#define RLIGHTS_IMPLEMENTATION
+#include "rlights.h"
