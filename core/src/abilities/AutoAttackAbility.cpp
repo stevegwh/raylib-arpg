7f15a69b68b053929abe8cf47271d52461a90199
stevegwh <sgwheeler@posteo.net>
2024-08-10 07:42:26 +0100

    auto attack now uses states

--- core/src/abilities/AutoAttackAbility.cpp
+++ core/src/abilities/AutoAttackAbility.cpp
@@ -7,6 +7,58 @@
 
 namespace sage
 {
+    void AutoAttackAbility::IdleState::Update(entt::entity self)
+    {
+        ability->cooldownTimer.Update(GetFrameTime());
+        if (ability->cooldownTimer.HasFinished())
+        {
+            ability->ChangeState(self, AbilityState::AWAITING_EXECUTION);
+        }
+        // if (ability->vfx->active)
+        // {
+        //     ability->vfx->Update(GetFrameTime());
+        // }
+    }
+
+    void AutoAttackAbility::IdleState::OnEnter(entt::entity self)
+    {
+        if (!ability->abilityData.repeatable)
+        {
+            ability->cooldownTimer.Stop();
+            auto& animation = ability->registry->get<Animation>(self);
+            animation.ChangeAnimationByEnum(AnimationEnum::IDLE, true);
+        }
+        else
+        {
+            ability->cooldownTimer.Restart();
+        }
+    }
+
+    void AutoAttackAbility::IdleState::Draw3D(entt::entity self)
+    {
+        // if (ability->vfx->active)
+        // {
+        //     ability->vfx->Draw3D();
+        // }
+    }
+
+    void AutoAttackAbility::AwaitingExecutionState::Update(entt::entity self)
+    {
+        ability->animationDelayTimer.Update(GetFrameTime());
+        if (ability->animationDelayTimer.HasFinished())
+        {
+            ability->Execute(self);
+        }
+    }
+
+    void AutoAttackAbility::AwaitingExecutionState::OnEnter(entt::entity self)
+    {
+        ability->cooldownTimer.Start();
+        ability->animationDelayTimer.Start();
+
+        auto& animation = ability->registry->get<Animation>(self);
+        animation.ChangeAnimationByEnum(AnimationEnum::AUTOATTACK, true);
+    }
 
     void AutoAttackAbility::Execute(entt::entity self)
     {
@@ -15,31 +67,38 @@ namespace sage
 
         auto target = registry->get<CombatableActor>(self).target;
         HitSingleTarget(registry, self, abilityData, target);
+        ChangeState(self, AbilityState::IDLE);
     }
 
     void AutoAttackAbility::Init(entt::entity self)
     {
-        cooldownTimer.Start();
+        ChangeState(self, AbilityState::AWAITING_EXECUTION);
     }
 
-    void AutoAttackAbility::Cancel()
+    void AutoAttackAbility::Cancel(entt::entity self)
     {
         cooldownTimer.Stop();
+        ChangeState(self, AbilityState::IDLE);
+    }
+
+    void AutoAttackAbility::Draw3D(entt::entity self)
+    {
+        state->Draw3D(self);
+        Ability::Draw3D(self);
     }
 
     void AutoAttackAbility::Update(entt::entity self)
     {
-        cooldownTimer.Update(GetFrameTime());
-        if (cooldownTimer.HasFinished())
-        {
-            Execute(self);
-            cooldownTimer.Restart();
-        }
+        state->Update(self);
     }
 
     AutoAttackAbility::AutoAttackAbility(
         entt::registry* _registry, AbilityData _abilityData)
         : Ability(_registry, _abilityData)
     {
+        states[AbilityState::IDLE] = std::make_unique<IdleState>(this);
+        states[AbilityState::AWAITING_EXECUTION] =
+            std::make_unique<AwaitingExecutionState>(this);
+        state = states[AbilityState::IDLE].get();
     }
 } // namespace sage
--- core/src/abilities/AutoAttackAbility.hpp
+++ core/src/abilities/AutoAttackAbility.hpp
@@ -6,14 +6,40 @@ namespace sage
 {
     class AutoAttackAbility : public Ability
     {
+        class IdleState : public State
+        {
+            AutoAttackAbility* ability;
+
+          public:
+            void OnEnter(entt::entity self) override;
+            void Update(entt::entity self) override;
+            void Draw3D(entt::entity self) override;
+            IdleState(AutoAttackAbility* _ability) : ability(_ability)
+            {
+            }
+        };
+
+        class AwaitingExecutionState : public State
+        {
+            AutoAttackAbility* ability;
+
+          public:
+            void OnEnter(entt::entity self) override;
+            void Update(entt::entity self) override;
+            AwaitingExecutionState(AutoAttackAbility* _ability) : ability(_ability)
+            {
+            }
+        };
+
       protected:
         AutoAttackAbility(entt::registry* _registry, AbilityData _abilityData);
 
       public:
         void Execute(entt::entity self) override;
         void Update(entt::entity self) override;
+        void Draw3D(entt::entity self) override;
         void Init(entt::entity self) override;
-        void Cancel();
+        void Cancel(entt::entity self);
         ~AutoAttackAbility() override = default;
     };
 } // namespace sage
