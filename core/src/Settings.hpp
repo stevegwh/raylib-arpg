d89566f128b506d2bf97dc80a4e5f5609e004b63
Steve Wheeler <sgwheeler@posteo.net>
2024-08-17 20:52:41 +0100

    Added floor fire vfx

--- core/src/Settings.hpp
+++ core/src/Settings.hpp
@@ -6,36 +6,37 @@
 
 #include "cereal/cereal.hpp"
 
-
-struct Settings
+namespace sage
 {
-	int screenWidth = 1280;
-	int screenHeight = 720;
+    struct Settings
+    {
+        int screenWidth = 1280;
+        int screenHeight = 720;
 
-	void ResetScreenSize()
-	{
-		screenWidth = SCREEN_WIDTH;
-		screenHeight = SCREEN_HEIGHT;
-	}
+        void ResetScreenSize()
+        {
+            screenWidth = SCREEN_WIDTH;
+            screenHeight = SCREEN_HEIGHT;
+        }
 
-	template <class Archive>
-	void save(Archive& archive) const
-	{
-		archive(CEREAL_NVP(SCREEN_WIDTH),
-		        CEREAL_NVP(SCREEN_HEIGHT));
-	}
+        template <class Archive>
+        void save(Archive& archive) const
+        {
+            archive(CEREAL_NVP(SCREEN_WIDTH), CEREAL_NVP(SCREEN_HEIGHT));
+        }
 
-	// TODO: Fairly sure below my logic is reversed (im saving and loading the default values, not the user settings)
-	template <class Archive>
-	void load(Archive& archive)
-	{
-		archive(CEREAL_NVP(SCREEN_WIDTH),
-		        CEREAL_NVP(SCREEN_HEIGHT));
-		screenWidth = SCREEN_WIDTH;
-		screenHeight = SCREEN_HEIGHT;
-	}
+        // TODO: Fairly sure below my logic is reversed (im saving and loading the default values, not the user
+        // settings)
+        template <class Archive>
+        void load(Archive& archive)
+        {
+            archive(CEREAL_NVP(SCREEN_WIDTH), CEREAL_NVP(SCREEN_HEIGHT));
+            screenWidth = SCREEN_WIDTH;
+            screenHeight = SCREEN_HEIGHT;
+        }
 
-private:
-	int SCREEN_WIDTH = 1280;
-	int SCREEN_HEIGHT = 720;
-};
+      private:
+        int SCREEN_WIDTH = 1280;
+        int SCREEN_HEIGHT = 720;
+    };
+} // namespace sage
--- core/src/abilities/vfx/FloorFireVFX.cpp
+++ core/src/abilities/vfx/FloorFireVFX.cpp
@@ -8,11 +8,6 @@
 
 #include "raylib.h"
 #include "raymath.h"
-#include "slib.hpp"
-
-#include <algorithm>
-#include <cmath>
-#include <cstdlib>
 #include <iostream>
 
 namespace sage
@@ -21,25 +16,21 @@ namespace sage
     void FloorFireVFX::Draw3D() const
     {
         BeginShaderMode(shader);
-        // Draw quad here (Just make it a renderable?)
+        //  Draw quad here (Just make it a renderable?)
         DrawModel(renderModel, targetPos, 1.0f, WHITE);
         EndShaderMode();
     }
 
     void FloorFireVFX::Update(float dt)
     {
-        time += 3 * dt;
+        time += 3 * GetFrameTime();
         SetShaderValue(shader, secondsLoc, &time, SHADER_UNIFORM_FLOAT);
     }
 
     void FloorFireVFX::InitSystem(const Vector3& _target)
     {
+        active = true;
         targetPos = _target;
-        secondsLoc = GetShaderLocation(shader, "seconds");
-        screenSizeLoc = GetShaderLocation(shader, "screenSize");
-
-        Vector2 screenSize = {(float)1280, (float)720}; // TODO: Guess I'll need to pass in settings here
-        SetShaderValue(shader, screenSizeLoc, &screenSize, SHADER_UNIFORM_VEC2);
     }
 
     FloorFireVFX::~FloorFireVFX()
@@ -52,7 +43,14 @@ namespace sage
 
     FloorFireVFX::FloorFireVFX(Camera* _camera) : VisualFX(_camera)
     {
-        shader = ResourceManager::ShaderLoad(nullptr, "resources/floorfirefx.fs");
-        renderModel = LoadModelFromMesh(GenMeshPlane(2.0f, 2.0f, 1, 1));
+        shader = LoadShader(nullptr, "resources/shaders/floorfirefx.fs");
+
+        renderModel = LoadModelFromMesh(GenMeshPlane(10.0f, 10.0f, 1, 1));
+        renderModel.materials[0].shader = shader;
+        secondsLoc = GetShaderLocation(shader, "seconds");
+        screenSizeLoc = GetShaderLocation(shader, "screenSize");
+
+        screenSize = {(float)1280, (float)720}; // TODO: Guess I'll need to pass in settings here
+        SetShaderValue(shader, screenSizeLoc, &screenSize, SHADER_UNIFORM_VEC2);
     }
 } // namespace sage
