26a8c325c76b98184cb4faeb7edd778b73b07878
stevegwh <sgwheeler@posteo.net>
2024-08-23 22:45:24 +0100

    Textures now unloaded correctly from renderable

--- core/src/components/Renderable.cpp
+++ core/src/components/Renderable.cpp
@@ -5,37 +5,32 @@
 
 #include "Renderable.hpp"
 
-#include <utility>
 #include <cstring>
+#include <utility>
 
 namespace sage
 {
-	Renderable::~Renderable()
-	{
-		if (shader.has_value())
-		{
-			UnloadShader(shader.value());
-		}
-		UnloadModel(model);
-		// TODO: Unload textures
-		//UnloadTexture(material.diffuse);
-	}
+    Renderable::~Renderable()
+    {
+        if (shader.has_value())
+        {
+            UnloadShader(shader.value());
+        }
+        UnloadModel(model);
+        for (auto& texture : textures)
+        {
+            UnloadTexture(texture);
+        }
+    }
 
-	Renderable::Renderable(
-		Model _model,
-		MaterialPaths _materials,
-		Matrix _localTransform)
-		:
-		initialTransform(_localTransform),
-		materials(std::move(_materials)),
-		model(_model)
-	{
-		model.transform = initialTransform;
-	}
+    Renderable::Renderable(Model _model, MaterialPaths _materials, Matrix _localTransform)
+        : initialTransform(_localTransform), materials(std::move(_materials)), model(_model)
+    {
+        model.transform = initialTransform;
+    }
 
-	Renderable::Renderable(Model _model, Matrix _localTransform) :
-		initialTransform(_localTransform), model(_model)
-	{
-		model.transform = initialTransform;
-	}
-}
+    Renderable::Renderable(Model _model, Matrix _localTransform) : initialTransform(_localTransform), model(_model)
+    {
+        model.transform = initialTransform;
+    }
+} // namespace sage
--- core/src/components/Renderable.hpp
+++ core/src/components/Renderable.hpp
@@ -9,74 +9,70 @@
 #include "raylib.h"
 #include "raymath.h"
 
-#include "raylib-cereal.hpp"
 #include "cereal/cereal.hpp"
+#include "raylib-cereal.hpp"
 
 #include "cereal/types/string.hpp"
 #include "ResourceManager.hpp"
 
-#include <string>
 #include <optional>
-
+#include <string>
+#include <vector>
 
 namespace sage
 {
-struct Renderable
-{
-	Color hint = WHITE;
-	bool active = true;
-    Matrix initialTransform{};
-    MaterialPaths materials;
-    Model model{}; // was const
-    std::optional<Shader> shader;
-    std::string name = "Default";
-    bool serializable = true;
-
-    Renderable() = default;
-    Renderable(const Renderable &) = delete;
-    Renderable &operator=(const Renderable &) = delete;
-    Renderable(Model _model, MaterialPaths _materials, Matrix _localTransform);
-    Renderable(Model _model, Matrix _localTransform);
-    ~Renderable();
-
-    template<class Archive>
-    void save(Archive &archive) const
+    struct Renderable
     {
-        archive(
-            model,
-            name,
-            materials,
-            initialTransform
-        );
-    }
+        std::vector<Texture> textures;
+        Color hint = WHITE;
+        bool active = true;
+        Matrix initialTransform{};
+        MaterialPaths materials;
+        Model model{}; // was const
+        std::optional<Shader> shader;
+        std::string name = "Default";
+        bool serializable = true;
 
-    template<class Archive>
-    void load(Archive &archive)
-    {
-        archive(
-            model,
-            name,
-            materials,
-            initialTransform
-        );
+        Renderable() = default;
+        Renderable(const Renderable&) = delete;
+        Renderable& operator=(const Renderable&) = delete;
+        Renderable(Model _model, MaterialPaths _materials, Matrix _localTransform);
+        Renderable(Model _model, Matrix _localTransform);
+        ~Renderable();
 
-        char *_name = new char[this->name.size() + 1];
-        model.meshes[0].name = _name;
-        model.transform = initialTransform;
-        
-        // TODO: Should use resource manager to load mesh data
+        template <class Archive>
+        void save(Archive& archive) const
+        {
+            archive(model, name, materials, initialTransform);
+        }
 
-        // Set the textures of the model with their respective paths
-        if (FileExists(materials.diffuse.c_str()))
-            model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture =
-                LoadTextureFromImage(ResourceManager::LoadTexture(materials.diffuse));
-        if (FileExists(materials.specular.c_str()))
-            model.materials[0].maps[MATERIAL_MAP_SPECULAR].texture =
-                LoadTextureFromImage(ResourceManager::LoadTexture(materials.specular));
-        if (FileExists(materials.normal.c_str()))
-            model.materials[0].maps[MATERIAL_MAP_NORMAL].texture =
-                LoadTextureFromImage(ResourceManager::LoadTexture(materials.normal));
+        template <class Archive>
+        void load(Archive& archive)
+        {
+            archive(model, name, materials, initialTransform);
 
-    }
-};
-}
+            char* _name = new char[this->name.size() + 1];
+            model.meshes[0].name = _name;
+            model.transform = initialTransform;
+
+            if (FileExists(materials.diffuse.c_str()))
+            {
+                auto texture = LoadTextureFromImage(ResourceManager::LoadTexture(materials.diffuse));
+                model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;
+                textures.push_back(texture);
+            }
+            if (FileExists(materials.specular.c_str()))
+            {
+                auto texture = LoadTextureFromImage(ResourceManager::LoadTexture(materials.specular));
+                model.materials[0].maps[MATERIAL_MAP_SPECULAR].texture = texture;
+                textures.push_back(texture);
+            }
+            if (FileExists(materials.normal.c_str()))
+            {
+                auto texture = LoadTextureFromImage(ResourceManager::LoadTexture(materials.normal));
+                model.materials[0].maps[MATERIAL_MAP_NORMAL].texture = texture;
+                textures.push_back(texture);
+            }
+        }
+    };
+} // namespace sage
