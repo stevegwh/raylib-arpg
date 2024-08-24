20244cf737c6ce41ebf40d0402a1c08f764ac24a
stevegwh <sgwheeler@posteo.net>
2024-08-24 18:36:41 +0100

    tidying up

--- core/src/EntityEventBridge.hpp
+++ core/src/EntityEventBridge.hpp
@@ -7,50 +7,49 @@
 
 namespace sage
 {
-
-    class BridgeBase
-    {
-      public:
-        virtual ~BridgeBase() {};
-    };
-
-    /*
-     * Below offers a way to hook onto mouse events (etc.) and publish the original signals but with a
-     * reference to the caller's entity id ("self") so the system can use the entity to get the component data.
-     */
-    template <typename... Args>
-    class EntityEventBridge : public BridgeBase
-    {
-      private:
-        entt::entity subscriber;
-        entt::connection connection;
-        entt::sigh<void(entt::entity, Args...)>* outSignal;
-
-        void onEventPublish(Args... args)
-        {
-            outSignal->publish(subscriber, args...);
-        }
-
-      public:
-        explicit EntityEventBridge(entt::entity subscriber) : subscriber(subscriber)
-        {
-        }
-
-        void BridgeEvents(entt::sigh<void(Args...)>& signal, entt::sigh<void(entt::entity, Args...)>& _outSignal)
-        {
-            entt::sink sink{signal};
-            connection = sink.template connect<&EntityEventBridge::onEventPublish>(*this);
-            outSignal = &_outSignal;
-        }
-
-        ~EntityEventBridge() override
-        {
-            connection.release();
-        }
-    };
-
     class BridgeManager
     {
+        class BridgeBase
+        {
+          public:
+            virtual ~BridgeBase() {};
+        };
+
+        /*
+         * Below offers a way to hook onto mouse events (etc.) and publish the original signals but with a
+         * reference to the caller's entity id ("self") so the system can use the entity to get the component data.
+         */
+        template <typename... Args>
+        class EntityEventBridge : public BridgeBase
+        {
+          private:
+            entt::entity subscriber;
+            entt::connection connection;
+            entt::sigh<void(entt::entity, Args...)>* outSignal;
+
+            void onEventPublish(Args... args)
+            {
+                outSignal->publish(subscriber, args...);
+            }
+
+          public:
+            explicit EntityEventBridge(
+                entt::entity subscriber,
+                entt::sigh<void(Args...)>& signal,
+                entt::sigh<void(entt::entity, Args...)>& _outSignal)
+                : subscriber(subscriber)
+            {
+                entt::sink sink{signal};
+                connection = sink.template connect<&EntityEventBridge::onEventPublish>(*this);
+                outSignal = &_outSignal;
+            }
+
+            ~EntityEventBridge() override
+            {
+                connection.release();
+            }
+        };
+
         std::vector<std::unique_ptr<BridgeBase>> bridges;
         int counter = -1;
 
@@ -61,8 +60,7 @@ namespace sage
             entt::sigh<void(Args...)>& inSignal,
             entt::sigh<void(entt::entity, Args...)>& outSignal)
         {
-            auto bridge = std::make_unique<EntityEventBridge<Args...>>(entity);
-            bridge->BridgeEvents(inSignal, outSignal);
+            auto bridge = std::make_unique<EntityEventBridge<Args...>>(entity, inSignal, outSignal);
             bridges.push_back(std::move(bridge));
             return ++counter;
         }
