54c86c97f3064cf728615d55e0c6d47c57b2d736
stevegwh <sgwheeler@posteo.net>
2024-08-29 21:54:18 +0100

    Finished refactoring abilities

--- core/src/abilities/AbilityResourceManager.cpp
+++ core/src/abilities/AbilityResourceManager.cpp
@@ -83,19 +83,23 @@ namespace sage
     }
 
     std::unique_ptr<AbilityFunction> AbilityResourceManager::GetExecuteFunc(
-        AbilityFunctionEnum name, entt::registry* _registry, entt::entity caster, GameData* _gameData)
+        AbilityFunctionEnum name,
+        entt::registry* _registry,
+        entt::entity caster,
+        entt::entity _abilityDataEntity,
+        GameData* _gameData)
     {
         if (name == AbilityFunctionEnum::SingleTargetHit)
         {
-            return std::make_unique<SingleTargetHitFunc>(_registry, caster, _gameData);
+            return std::make_unique<SingleTargetHitFunc>(_registry, caster, _abilityDataEntity, _gameData);
         }
         else if (name == AbilityFunctionEnum::MultihitRadiusFromCaster)
         {
-            return std::make_unique<MultihitRadiusFromCaster>(_registry, caster, _gameData);
+            return std::make_unique<MultihitRadiusFromCaster>(_registry, caster, _abilityDataEntity, _gameData);
         }
         else if (name == AbilityFunctionEnum::MultihitRadiusFromCursor)
         {
-            return std::make_unique<MultihitRadiusFromCursor>(_registry, caster, _gameData);
+            return std::make_unique<MultihitRadiusFromCursor>(_registry, caster, _abilityDataEntity, _gameData);
         }
         return nullptr;
     }
