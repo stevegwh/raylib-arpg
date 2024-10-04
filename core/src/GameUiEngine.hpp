43ede44e22f4fd7aa773b17db65fcbbe00b50554
Steve Wheeler <sgwheeler@posteo.net>
2024-10-04 15:14:15 +0100

    Working on UI

--- core/src/GameUiEngine.hpp
+++ core/src/GameUiEngine.hpp
@@ -15,76 +15,120 @@ namespace sage
     class UserInput;
     class Cursor;
 
-    struct Panel;
+    struct Table;
+    struct TableRow;
+    struct TableCell;
+    struct UIElement;
+
+    class FloatConstrained
+    {
+        float value = 0;
+        float min = 0;
+        float max = 100;
+
+      public:
+        FloatConstrained(float _value, float _min, float _max) : value(_value), min(_min), max(_max){};
+        FloatConstrained() = default;
+
+        [[nodiscard]] float GetValue() const
+        {
+            return value;
+        }
+    };
 
     struct Window
     {
-        int id;
-        Vector2 pos;
         Rectangle rec;
         Texture tex;
         NPatchInfo nPatchInfo;
         entt::sigh<void(int)> onWindowStartHover;
         entt::sigh<void(int)> onWindowEndHover;
-        std::vector<Panel> children;
+        std::vector<Table> children;
+
+        Table* CreateTable();
+
+        void Draw2D();
     };
 
-    struct Panel
+    struct Table
     {
-        int id;
         Window* parent;
-        Vector2 pos;
         Rectangle rec;
+        FloatConstrained width;
+        FloatConstrained height;
         Texture tex;
+        std::vector<TableRow> children;
+        TableRow* CreateTableRow();
+        void Draw2D();
     };
 
-    struct TextBox
+    struct TableRow
     {
-        int id;
-        int parent;
+        Table* parent;
+        Rectangle rec;
+        FloatConstrained width;
+        FloatConstrained height;
+        Texture tex;
+        std::vector<TableCell> children;
+        TableCell* CreateTableCell(float width);
+        void Draw2D();
+    };
+
+    struct UIElement
+    {
+        virtual void Draw2D() = 0;
+    };
+
+    struct TextBox : public UIElement
+    {
         float fontSize;
-        Vector2 pos;
         Rectangle rec;
         // font?
         // color?
         std::string content;
         // Text wrap, scroll bar etc?
+
+        void Draw2D() override;
     };
 
-    struct Button
+    struct Button : public UIElement
     {
-        int id;
-        int parent;
         Texture tex;
-        Vector2 pos;
         Rectangle rec;
-        float w;
-        float h;
         entt::sigh<void(int)> onButtonStartHover;
         entt::sigh<void(int)> onButtonEndHover;
         entt::sigh<void(int)> onButtonPress;
+
+        void Draw2D() override;
+    };
+
+    struct TableCell
+    {
+        TableRow* parent;
+        Rectangle rec;
+        FloatConstrained width;
+        FloatConstrained height;
+        float paddingLeft, paddingRight, paddingUp, paddingDown;
+        float marginLeft, marginRight, marginUp, marginDown;
+        Texture tex;
+        TextBox* CreateTextbox(const std::string& _content);
+        Button* CreateButton(Texture _tex);
+        std::vector<std::unique_ptr<UIElement>> children;
+        void Draw2D();
     };
 
     class GameUIEngine
     {
         Texture nPatchTexture;
         std::vector<Window> windows;
-        std::vector<TextBox> textboxes;
-        std::vector<Button> buttons;
         int nextId;
 
       public:
-        int CreateWindow(Vector2 pos, float w, float h);
-        int CreatePanel(Vector2 pos, float w, float h, int parent);
-        int CreateTextbox(const std::string& content, Vector2 pos, float w, float h, int parent);
-        int CreateButton(Texture tex, Vector2 pos, float w, float h, int parent);
+        Window* CreateWindow(Vector2 pos, float w, float h);
 
         void Draw2D();
         void Update();
 
         GameUIEngine(Settings* _settings, UserInput* _userInput, Cursor* _cursor);
-
-      private:
-        Window* GetWindowById(int id); // Add this line
     };
 } // namespace sage
