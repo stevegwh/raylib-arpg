614e55bbd61a26a5ac2eaa189f2b7779123b268d
Steve Wheeler <sgwheeler@posteo.net>
2024-10-05 15:50:31 +0100

    Almost working

--- core/src/GameUiFactory.cpp
+++ core/src/GameUiFactory.cpp
@@ -14,11 +14,20 @@ namespace sage
         auto table = window->CreateTable();
         auto row = table->CreateTableRow();
         auto cell = row->CreateTableCell();
-        auto cell2 = row->CreateTableCell();
-        auto cell3 = row->CreateTableCell();
+        cell->padding.left = 10;
         cell->CreateTextbox("Number 1!");
+        auto cell2 = row->CreateTableCell();
         cell2->CreateTextbox("Number 2!");
+        auto cell3 = row->CreateTableCell();
         cell3->CreateTextbox("Number 3!");
+
+        auto row2 = table->CreateTableRow();
+        auto cell4 = row2->CreateTableCell();
+        cell4->CreateTextbox("Number 4!");
+        auto cell5 = row2->CreateTableCell();
+        cell5->CreateTextbox("Number 5!");
+        //        auto cell3 = row->CreateTableCell();
+        //        cell3->CreateTextbox("Number 3!");
     }
 
     void GameUiFactory::CreateInventoryWindow(GameUIEngine* engine, Vector2 pos, float w, float h)
