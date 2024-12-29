d899454268bc2d4d8ca99fe91a080e07be46f196
stevegwh <sgwheeler@posteo.net>
2024-12-29 12:19:23 +0000

    Updated license information for Slibmodel

--- core/utils/Slibmodel.cpp
+++ core/utils/Slibmodel.cpp
@@ -2,6 +2,33 @@
 // Created by Steve Wheeler on 26/12/2024.
 //
 
+/**
+ * This (WIP) file is a modified version of rmodels.c. The original license is included below.
+ *
+ * /**********************************************************************************************
+ *
+ *   rmodels - Basic functions to draw 3d shapes and load and draw 3d models
+ *   LICENSE: zlib/libpng
+ *
+ *   Copyright (c) 2013-2024 Ramon Santamaria (@raysan5)
+ *
+ *   This software is provided "as-is", without any express or implied warranty. In no event
+ *   will the authors be held liable for any damages arising from the use of this software.
+ *
+ *   Permission is granted to anyone to use this software for any purpose, including commercial
+ *   applications, and to alter it and redistribute it freely, subject to the following restrictions:
+ *
+ *     1. The origin of this software must not be misrepresented; you must not claim that you
+ *     wrote the original software. If you use this software in a product, an acknowledgment
+ *     in the product documentation would be appreciated but is not required.
+ *
+ *     2. Altered source versions must be plainly marked as such, and must not be misrepresented
+ *     as being the original software.
+ *
+ *     3. This notice may not be removed or altered from any source distribution.
+ *
+ **********************************************************************************************/
+
 #include "Slibmodel.hpp"
 
 // Check if config flags have been externally provided on compilation line
@@ -10,7 +37,7 @@
 #endif
 
 #if defined(SUPPORT_MODULE_RMODELS)
-
+#include "raylib.h"
 #include "raymath.h" // Required for: Vector3, Quaternion and Matrix functionality
 #include "rlgl.h"    // OpenGL abstraction layer to OpenGL 1.1, 2.1, 3.3+ or ES2
 #include "utils.h"   // Required for: TRACELOG(), LoadFileData(), LoadFileText(), SaveFileText()
