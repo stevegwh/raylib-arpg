df27b163a05b8aa2a59189aa54097cf5cb48117c
Steve Wheeler <sgwheeler@posteo.net>
2024-12-29 22:22:15 +0000

    Door opens.

--- core/src/components/DoorBehaviorComponent.cpp
+++ core/src/components/DoorBehaviorComponent.cpp
@@ -10,6 +10,7 @@
 namespace sage
 {
 
+    // TODO: This is essentially hard coded to open the sole door in the game right now.
     void DoorBehaviorComponent::ExecuteBehavior(entt::entity clicked)
     {
         if (clicked != self || locked) return;
@@ -18,16 +19,14 @@ namespace sage
 
         const auto& [rotx, roty, rotz] = transform->GetLocalRot();
 
-        if (!open) // Opening the door
+        if (!open)
         {
-            // If facing positive Z, rotate one way; if facing negative Z, rotate the other
             float targetRotation = (transform->forward().z > 0) ? openYRotation : -openYRotation;
             transform->SetLocalRot(Vector3{rotx, targetRotation, rotz});
             open = true;
         }
-        else // Closing the door
+        else
         {
-            // Always return to the original rotation when closing
             transform->SetLocalRot(Vector3{rotx, closedRotation, rotz});
             open = false;
         }
