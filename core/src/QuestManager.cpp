084bd6c2799b09a132c9e23d8afd1c8dfc157bee
stevegwh <sgwheeler@posteo.net>
2025-01-05 01:08:22 +0000

    Moved a yaml style format for the quests

--- core/src/QuestManager.cpp
+++ core/src/QuestManager.cpp
@@ -34,49 +34,63 @@ namespace sage
                 auto& quest = registry->get<Quest>(createQuest(questName));
 
                 // TODO: Must make sure renderable and item systems are intialised before this
-                if (file.is_open())
+                if (!file.is_open()) return;
+
+                std::string buff;
+                while (std::getline(file, buff, '\n'))
                 {
-                    std::string description;
-                    std::string buff;
-                    while (std::getline(file, buff, '\n'))
+                    if (buff.find("[Meta]") != std::string::npos)
                     {
-                        if (buff.find("title: ") != std::string::npos)
+                        std::string metaLine;
+                        while (std::getline(file, metaLine) && !metaLine.empty())
                         {
-                            auto sub = buff.substr(std::string("title: ").size());
-                            quest.journalTitle = sub;
-                        }
-                        else if (buff == "---")
-                        {
-                            std::string contentLine;
-                            while (std::getline(file, contentLine) && contentLine != "---")
+                            if (metaLine.find("title: ") != std::string::npos)
                             {
-                                description += contentLine + "\n";
+                                auto sub = metaLine.substr(std::string("title: ").size());
+                                quest.journalTitle = sub;
                             }
-                            quest.journalDescription = description;
-                        }
-                        else if (buff.find("dialog: ") != std::string::npos)
-                        {
-                            auto sub = buff.substr(std::string("dialog: ").size());
-                            auto speakToEntity = gameData->renderSystem->FindRenderable(sub);
-                            assert(speakToEntity != entt::null);
-                            registry->emplace<QuestTaskComponent>(speakToEntity, questName);
-                            quest.AddTask(speakToEntity);
-                            assert(registry->any_of<DialogComponent>(speakToEntity));
-                        }
-                        else if (buff.find("item: ") != std::string::npos)
-                        {
-                            auto sub = buff.substr(std::string("item: ").size());
-                            auto pickupEntity = gameData->renderSystem->FindRenderable(sub);
-                            assert(pickupEntity != entt::null);
-                            registry->emplace<QuestTaskComponent>(pickupEntity, questName);
-                            quest.AddTask(pickupEntity);
-                            assert(registry->any_of<ItemComponent>(pickupEntity));
+                            else if (metaLine.find("giver: ") != std::string::npos)
+                            {
+                                auto sub = metaLine.substr(std::string("giver: ").size());
+                                // Do something here
+                            }
                         }
                     }
-                }
-                else
-                {
-                    assert(0);
+                    else if (buff.find("[Description]") != std::string::npos)
+                    {
+                        std::string description;
+                        std::string descriptionLine;
+                        while (std::getline(file, descriptionLine) && !descriptionLine.empty())
+                        {
+                            description += descriptionLine + "\n";
+                        }
+                        quest.journalDescription = description;
+                    }
+                    else if (buff.find("[Tasks]") != std::string::npos)
+                    {
+                        std::string taskLine;
+                        while (std::getline(file, taskLine) && !taskLine.empty())
+                        {
+                            if (taskLine.find("dialog: ") != std::string::npos)
+                            {
+                                auto sub = taskLine.substr(std::string("dialog: ").size());
+                                auto speakToEntity = gameData->renderSystem->FindRenderable(sub);
+                                assert(speakToEntity != entt::null);
+                                registry->emplace<QuestTaskComponent>(speakToEntity, questName);
+                                quest.AddTask(speakToEntity);
+                                assert(registry->any_of<DialogComponent>(speakToEntity));
+                            }
+                            else if (taskLine.find("item: ") != std::string::npos)
+                            {
+                                auto sub = taskLine.substr(std::string("item: ").size());
+                                auto pickupEntity = gameData->renderSystem->FindRenderable(sub);
+                                assert(pickupEntity != entt::null);
+                                registry->emplace<QuestTaskComponent>(pickupEntity, questName);
+                                quest.AddTask(pickupEntity);
+                                assert(registry->any_of<ItemComponent>(pickupEntity));
+                            }
+                        }
+                    }
                 }
             }
         }
